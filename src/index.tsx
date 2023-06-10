7660973e77790a920ebbb6e9cfcab7d345f477ca
Saivan <saivan@me.com>
2023-06-10 23:23:57 +1000

    We can now save svgs to the clipboard

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
 
-import { Detail, List, render } from "@raycast/api"
+import { ActionPanel, List, Action } from "@raycast/api"
 import { useEffect, useState } from "react"
 
-import { renderSvg, typesetPath } from "./math-server"
-
-
+import { exportPngImage, exportSvgImage, renderSvg, typesetBase64Svg } from "./math-server"
 
 export default function Command( props: { }) {
   const [searchText, setSearchText] = useState("")
+
+  function getMarkdown(asciimath: boolean, inline: boolean) {
+    const svgBase64 = typesetBase64Svg(searchText, asciimath, inline)
+    const markdown = `![${searchText}](${svgBase64})`
+    return markdown
+  }
+
+  function getActions(asciimath: boolean, inline: boolean) {
+    const actionPanel = 
+      <ActionPanel title="Save Mathematics">
+        <Action.CopyToClipboard
+          title="Copy the svg to the clipboard"
+          content={{file: exportSvgImage(searchText, asciimath, inline)}}
+        />
+        {/* <Action.CopyToClipboard
+          title="Copy a png to the clipboard"
+          content={{file: exportPngImage(searchText, asciimath, inline)}}
+        /> */}
+        <Action.OpenInBrowser 
+          url="https://github.com/raycast/extensions/pull/1" />
+      </ActionPanel>
+    return actionPanel
+  }
+
   return (
     <List 
       isShowingDetail
@@ -19,38 +41,26 @@ export default function Command( props: { }) {
       <List.Item
         title="AsciiMath"
         subtitle="display"
-        detail={
-          <List.Item.Detail 
-            markdown={`![${searchText}](${renderSvg(searchText, true, false)})`} 
-          />
-        }
+        detail={ <List.Item.Detail markdown={getMarkdown(true, false)} /> }
+        actions={getActions(true, false) }
       />
       <List.Item
         title="Latex"
         subtitle="display"
-        detail={
-          <List.Item.Detail 
-            markdown={`![${searchText}](${renderSvg(searchText, false, false)})`} 
-          />
-        }
+        detail={ <List.Item.Detail markdown={getMarkdown(false, false)} /> }
+        actions={getActions(false, false) }
       />
       <List.Item
         title="AsciiMath"
         subtitle="inline"
-        detail={
-          <List.Item.Detail 
-            markdown={`<img src="${renderSvg(searchText, true, true)}" />`}
-          />
-        }
+        detail={ <List.Item.Detail markdown={getMarkdown(true, true)} /> }
+        actions={getActions(true, true) }
       />
       <List.Item
         title="Latex"
         subtitle="inline"
-        detail={
-          <List.Item.Detail 
-            markdown={`<img src="${renderSvg(searchText, false, true)}" />`}
-          />
-        }
+        detail={ <List.Item.Detail markdown={getMarkdown(false, true)} /> }
+        actions={getActions(true, false) }
       />
     </List>
   ) }
--- src/math-server.tsx
+++ src/math-server.tsx
@@ -1,22 +1,89 @@
 
 import { jax } from './mathjax'
 import asciiMathToLatex from 'asciimath-to-latex'
+import { Blob } from 'node:buffer'
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+import svg2img from 'svg2img'
+// import parseSvg from 'svg-parser'
 
 
-export function renderSvg (string, useAsciimath, inline) {
-  if (!string) return '';
-  const texString = useAsciimath ? asciiMathToLatex(string) : string;
-  const adaptor = jax.startup.adaptor;
-  const node = jax.tex2svg(texString, { display: !inline });
-  const svg = node.children[0];
-  svg.attributes.height = '200px'
-  svg.attributes.width = '100%'
-  const htmlCode = adaptor.outerHTML(svg);
-  const base64 = Buffer.from(htmlCode).toString('base64')
-  return `data:image/svg+xml;base64,${base64}`
+
+// export async function exportPngImage (
+//   string: string,
+//   useAsciimath: boolean,
+//   inline: boolean,
+// ) {
+//   // Get and parse the svg
+//   const svgString = renderSvg(string, useAsciimath, inline)
+//   const svgTree = parseSvg(svgString)
+//   const svgRoot = svgTree.children.find(node => node.tagName === 'svg');
+
+//   // Figure out the svg width and height
+//   const width = svgRoot.properties.width || 100;
+//   const height = svgRoot.properties.height || 100;
+
+//   // Convert it to png
+//   const directory = os.tmpdir()
+//   const filename = path.join(directory, `math.png`)
+//   const result = await new Promise(accept => {
+//     svg2img(svgString, { width, height }, (error, buffer) => {
+//       if (error) {
+//         console.error(`Error rendering SVG to PNG: ${error}`);
+//       } else {
+//         fs.writeFileSync(filename, buffer);
+//         console.log(`PNG file written to ${filename}`);
+//       }
+//     });
+
+//   })
+// }
+
+
+export function exportSvgImage (
+  string: string, 
+  useAsciimath:boolean, 
+  inline: boolean,
+) {
+  const svgString = renderSvg(string, useAsciimath, inline)
+  const directory = os.tmpdir()
+  const filename = path.join(directory, `math.svg`)
+  fs.writeFileSync(filename, svgString)
+  return filename
 }
 
 
+export function typesetBase64Svg (
+  string: string, 
+  useAsciimath:boolean, 
+  inline: boolean,
+) {
+  const htmlCode = renderSvg(string, useAsciimath, inline)
+  const base64 = Buffer.from(htmlCode).toString('base64')
+  const imgSrc = `data:image/svg+xml;base64,${base64}`
+  return imgSrc
+}
 
 
+export function renderSvg (
+  string: string, 
+  useAsciimath: boolean, 
+  inline: boolean,
+) {
+  // Given no string, return no source
+  if (!string) return ''
+
+  // Generate the svg from the document
+  const texString = useAsciimath ? asciiMathToLatex(string) : string
+  const adaptor = jax.startup.adaptor
+  const node = jax.tex2svg(texString, { display: !inline })
+
+  // Get the svg and modify its attributes
+  const svg = node.children[0]
+  svg.attributes.height = '200px'
+  svg.attributes.width = '100%'
+  const svgCode = adaptor.outerHTML(svg)
+  return svgCode
+}
 
